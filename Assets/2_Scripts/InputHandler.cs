1c030d46b3e6c149eea6665d3fb25101b56d0b0e
juno11234 <cozam2@naver.com>
2025-11-03 16:07:43 +0900

    Initial commit

---
+++ Assets/2_Scripts/InputHandler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public float horizontal;
+    public float vertical;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
